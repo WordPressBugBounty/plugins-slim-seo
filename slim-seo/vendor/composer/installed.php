fd9d928c18537600ea9ed72fc9211c693b10c76c
Sean Murphy <sean@iamseanmurphy.com>
2024-09-22 11:37:37 +0000

    3.25.3

---
+++ slim-seo/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elightup/slim-seo',
+        'pretty_version' => '3.25.3',
+        'version' => '3.25.3.0',
+        'reference' => '16f7d8ea57418424c95d4a4dbddc677ad8919c48',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elightup/slim-seo' => array(
+            'pretty_version' => '3.25.3',
+            'version' => '3.25.3.0',
+            'reference' => '16f7d8ea57418424c95d4a4dbddc677ad8919c48',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
