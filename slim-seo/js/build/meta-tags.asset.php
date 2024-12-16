ba200a189b2251de6d31f69e1ba5e8312eeb0f6c
Sean Murphy <sean@iamseanmurphy.com>
2024-12-16 05:17:06 +0000

    4.2.1

--- slim-seo/js/build/meta-tags.asset.php
+++ slim-seo/js/build/meta-tags.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '78e1bebc5f81daa815db');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '4df98d28f6cd606610c8');
--- slim-seo/vendor/composer/installed.php
+++ slim-seo/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'elightup/slim-seo',
-        'pretty_version' => '4.2.0',
-        'version' => '4.2.0.0',
-        'reference' => 'f392377078e384a5e921139519b7906777028ade',
+        'pretty_version' => '4.2.1',
+        'version' => '4.2.1.0',
+        'reference' => '4cdb4cb023bf2665e9489b8030a5350fd06256d2',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'elightup/slim-seo' => array(
-            'pretty_version' => '4.2.0',
-            'version' => '4.2.0.0',
-            'reference' => 'f392377078e384a5e921139519b7906777028ade',
+            'pretty_version' => '4.2.1',
+            'version' => '4.2.1.0',
+            'reference' => '4cdb4cb023bf2665e9489b8030a5350fd06256d2',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
