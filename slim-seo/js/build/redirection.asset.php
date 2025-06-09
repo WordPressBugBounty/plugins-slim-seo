69d1b5b3971048ef2afe32a5fdc3d374d408510d
Sean Murphy <sean@iamseanmurphy.com>
2025-06-09 15:17:53 +0000

    4.5.5

--- slim-seo/js/build/redirection.asset.php
+++ slim-seo/js/build/redirection.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c97aa3455473eefd2e11');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6b42043a7d971a7c2877');
--- slim-seo/vendor/composer/installed.php
+++ slim-seo/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'elightup/slim-seo',
-        'pretty_version' => '4.5.4',
-        'version' => '4.5.4.0',
-        'reference' => 'fb96bee870d908c8dd88f3002c6d27c5e9b0793e',
+        'pretty_version' => '4.5.5',
+        'version' => '4.5.5.0',
+        'reference' => 'c93427a7d1492e4b37e80e14665058fece5e438f',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'elightup/slim-seo' => array(
-            'pretty_version' => '4.5.4',
-            'version' => '4.5.4.0',
-            'reference' => 'fb96bee870d908c8dd88f3002c6d27c5e9b0793e',
+            'pretty_version' => '4.5.5',
+            'version' => '4.5.5.0',
+            'reference' => 'c93427a7d1492e4b37e80e14665058fece5e438f',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
