7d10f241d25f81eaad0de8220b2e06fdb3e7148a
Sean Murphy <sean@iamseanmurphy.com>
2025-06-30 04:26:55 +0000

    4.6.0

--- slim-seo/vendor/composer/installed.php
+++ slim-seo/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'elightup/slim-seo',
-        'pretty_version' => '4.5.7',
-        'version' => '4.5.7.0',
-        'reference' => '0da3ffcbd0f2a8d29ef025ac3974326bf7a063d5',
+        'pretty_version' => '4.6.0',
+        'version' => '4.6.0.0',
+        'reference' => 'a70a6a659d1a30197b74ea6254bbdf8002c435b2',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'elightup/slim-seo' => array(
-            'pretty_version' => '4.5.7',
-            'version' => '4.5.7.0',
-            'reference' => '0da3ffcbd0f2a8d29ef025ac3974326bf7a063d5',
+            'pretty_version' => '4.6.0',
+            'version' => '4.6.0.0',
+            'reference' => 'a70a6a659d1a30197b74ea6254bbdf8002c435b2',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'elightup/slim-twig' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '04ebb63b2d6c857ce842a1f089803abd4a020d80',
+            'reference' => 'c5ced434e08cb5d29175d17718ecc4562165f8cb',
             'type' => 'library',
             'install_path' => __DIR__ . '/../elightup/slim-twig',
             'aliases' => array(
